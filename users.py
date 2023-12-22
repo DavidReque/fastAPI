3dbaf91c37073d889eff44d7e4312c8ef93886b6
David Requeno <davidrequeno52@gmail.com>
2023-12-21 23:08:33 -0600

    Path y Query

--- users.py
+++ users.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     lastname: str
@@ -11,12 +12,14 @@ class User(BaseModel):
 
 user_list = [
         User(
+            id=1,
             name="David",
             surname="Davv",
             lastname="Requeno",
             age=19
         ),
         User(
+            id=2,
             name="Juan",
             surname="j",
             lastname="G",
@@ -31,4 +34,20 @@ async def usersjson():
 @app.get("/users")
 async def users():
     return user_list
+
+#Path
+@app.get("/user/{id}")
+async def user(id: int):
+        return search_user(id)
             
+#Query
+@app.get("/userquery/")
+async def user(id: int):
+    return search_user(id)
+        
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, user_list)
+    try:
+        return list(users)[0]
+    except: 
+        return {"error": "No se ha encontrado el usuario"}
