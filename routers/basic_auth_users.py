2872631babf83ae29cc1682e220980eb072bece3
David Requeno <davidrequeno52@gmail.com>
2024-01-04 23:06:07 -0600

    auth

---
+++ routers/basic_auth_users.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    username: str
+    full_name: str
+    email: str
+    disabled: bool
+
+user_db = {
+    
+}
