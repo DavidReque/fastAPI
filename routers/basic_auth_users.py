570f8eb5e67309895c17250df0475f447f5232cd
David Requeno <davidrequeno52@gmail.com>
2024-01-04 23:12:28 -0600

    user_db

--- routers/basic_auth_users.py
+++ routers/basic_auth_users.py
@@ -9,6 +9,22 @@ class User(BaseModel):
     email: str
     disabled: bool
 
+class UserDB(User):
+    password: str
+
 user_db = {
-    
+    "David": {
+        "username": "David",
+        "full_name": "Requeno",
+        "email": "dr@gmail.com",
+        "disabled": False,
+        "password": "david123"
+    },
+    "Juan": {
+        "username": "Juan",
+        "full_name": "J",
+        "email": "juan@gmail.com",
+        "disabled": True,
+        "password": "juan123"
+    },
 }
