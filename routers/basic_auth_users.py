2764b33a98154d2f62739a10cbe73c6986095d2e
David Requeno <davidrequeno52@gmail.com>
2024-01-05 22:39:26 -0600

    post

--- routers/basic_auth_users.py
+++ routers/basic_auth_users.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
 class User(BaseModel):
     username: str
@@ -32,3 +35,19 @@ users_db = {
 def search_user(username: str):
     if username in users_db:
         return UserDB(users_db[username])
+    
+@app.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends):
+    user_db = users_db.get(form.username)
+    if not user_db: 
+        raise HTTPException(
+            status_code=400, detail="El usuario no es correcto"
+        )
+    
+    user = search_user(form.username)
+    if not form.password == user.password:
+        raise HTTPException(
+            status_code=400, detail="la contraseña no es correcta"
+        )
+    
+    return {}
