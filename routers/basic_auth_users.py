85f02812bfbd76a6faa8fe49fb6204f5e4203111
David Requeno <davidrequeno52@gmail.com>
2024-01-06 21:36:39 -0600

    current_user

--- routers/basic_auth_users.py
+++ routers/basic_auth_users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
@@ -36,6 +36,16 @@ def search_user(username: str):
     if username in users_db:
         return UserDB(users_db[username])
     
+async def current_user(token: str = Depends(oauth2)):
+    user = search_user(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Credenciales de autenticacion invalidas", 
+            headers={"WWW-Authenticate", "Bearer"}
+        )
+    return user
+
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
@@ -51,3 +61,7 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
         )
     
     return {"access_token": user.username, "token_type": "bearer"}
+
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
