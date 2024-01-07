23f08d2fd71339a2be287793b32b41f82300ebfa
David Requeno <davidrequeno52@gmail.com>
2024-01-06 21:42:23 -0600

    **

--- routers/basic_auth_users.py
+++ routers/basic_auth_users.py
@@ -34,7 +34,7 @@ users_db = {
 
 def search_user(username: str):
     if username in users_db:
-        return UserDB(users_db[username])
+        return UserDB(**users_db[username])
     
 async def current_user(token: str = Depends(oauth2)):
     user = search_user(token)
