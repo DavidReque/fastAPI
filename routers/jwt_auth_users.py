421dc7566ca073c763f62f994a71bb989919a6eb
David Requeno <davidrequeno52@gmail.com>
2024-01-09 22:26:27 -0600

    ACCESS_TOKEN_DURATION

--- routers/jwt_auth_users.py
+++ routers/jwt_auth_users.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt
+from passlib.context import CryptContext
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_DURATION = 1
 
 app = FastAPI()
 
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+
+crypt = CryptContext(schemes=["bcrypt"])
 
 class User(BaseModel):
     username: str
@@ -21,13 +28,34 @@ users_db = {
         "full_name": "Requeno",
         "email": "dr@gmail.com",
         "disabled": False,
-        "password": "david123"
+        "password": "$2a$04$S78QVDqWsLEkafpe1Rj4NOSh3/Fw9UwmSIt1owdUlRRRfhnQRxeN"
     },
     "Juan": {
         "username": "Juan",
         "full_name": "J",
         "email": "juan@gmail.com",
         "disabled": True,
-        "password": "juan123"
+        "password": "$2a$04$dGOpvQWYZfJaUY1UjjdPm.SWBCWrIp/PPFVoiXG27FMrT62nz8f02"
     },
 }
+
+def search_user_db(username: str):
+    if username in users_db:
+        return UserDB(**users_db[username])
+
+@app.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    user_db = users_db.get(form.username)
+    if not user_db: 
+        raise HTTPException(
+            status_code=400, detail="El usuario no es correcto"
+        )
+    
+    user = search_user_db(form.username)
+
+    if not crypt.verify(form.password, user.password):
+        raise HTTPException(
+            status_code=400, detail="la contraseña no es correcta"
+        )
+    
+    return {"access_token": user.username, "token_type": "bearer"}
