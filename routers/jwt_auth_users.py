d44697c7cf84604c57cb509f50d872acfd8ee3f4
David Requeno <davidrequeno52@gmail.com>
2024-01-12 22:57:19 -0600

    token

--- routers/jwt_auth_users.py
+++ routers/jwt_auth_users.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_DURATION = 1
+SECRET = "228ec9c36cd92b8d4b7cd5e81456b5d994fd1d7251442ce7f03a3138f54a8052"
 
 app = FastAPI()
 
@@ -40,9 +41,40 @@ users_db = {
     },
 }
 
-def search_user_db(username: str):
+def search_user_db(username: str = Depends(oauth2)):
     if username in users_db:
         return UserDB(**users_db[username])
+    
+def search_user(username: str):
+    if username in users_db:
+        return User(**users_db[username])
+    
+async def auth_user(token: str):
+
+    exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Credenciales de autenticacion invalidas", 
+            headers={"WWW-Authenticate", "Bearer"}
+        ) 
+
+    try:
+        username = jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get("sub")
+        if username is None:
+            raise exception
+
+    except JWTError:
+               raise exception
+    
+    return search_user(username)
+    
+async def current_user(user: User = Depends(auth_user)):
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Usuario inactivo", 
+        )
+    
+    return user
 
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
@@ -62,7 +94,10 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     access_token = {"sub": user.username, 
                     "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)}
     
-    encoded_jwt = jwt.encode(access_token, "secret", algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(access_token, SECRET, algorithm=ALGORITHM)
 
     return {"access_token": encoded_jwt, "token_type": "bearer", "info": access_token}
 
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
